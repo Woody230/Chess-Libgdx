f78bef9487c4e9fb7169624e42cec6bcf33d7022
Brandon Selzer <bselzer1@hotmail.com>
2020-06-12 22:32:22 -0400

    Initial commit

---
+++ android/src/com/outlook/bselzer1/chess/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.outlook.bselzer1.chess;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.outlook.bselzer1.chess.GdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxGame(), config);
+	}
+}
